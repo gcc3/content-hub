2f4f8106210a03867c154c4dc369ebb14bdba100
Heyang Liu <lhypds@gmail.com>
2023-05-21 15:38:00 +0000

    Initial commit

---
+++ git.sh
@@ -0,0 +1,7 @@
+
+#!/usr/bin/bash
+
+# execute it with
+# wget -O - http://gcc3.com/git.sh | bash
+
+git config --global credential.helper store
---
+++ i.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/bash
+
+# for ubuntu initial setup
+# execute it with
+# wget -O - http://gcc3.com/i.sh | bash
+
+# color defination
+Color_Off='\033[0m'       # Text Reset
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# start
+echo -e "Hi, this is ${Purple}http://gcc3.com${Color_Off}"
+
+# detect distribution
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    distro=$NAME
+elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    distro=$DISTRIB_ID
+else
+    distro=$(uname -s)
+fi
+
+tools="curl vim git mlocate tree tmux ncdu"
+toolsBuild="build-essential"
+toolsConnct="openssh-server"
+
+# install basic tools
+if [ "$distro" == "Ubuntu" ]; then
+    echo "Detect Ubuntu, installing..."
+    sudo apt install $tools -y
+    sudo apt install $toolsBuild -y
+    sudo apt install $toolsConnct -y
+elif [ "$distro" == "Debian GNU/Linux" ]; then
+    echo "Detect Debian"
+    echo "Abort as Debian is not supported yet"
+elif [ "$distro" == "Fedora" ]; then
+    echo "Detect Fedora"
+    echo "Abort as Fedora is not supported yet"
+elif [ "$distro" == "CentOS Linux" ]; then
+    echo "Detect CentOS, installing..."
+    sudo yum install $tools -y
+    sudo yum install $toolsBuild -y
+    sudo yum install $toolsConnct -y
+else
+    echo "Unknown distribution: $distro"
+fi
+
+# finish
+echo "Finished🍺"
