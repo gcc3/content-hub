f2ab9d1922868a774e0e4279e8b907855cf5a5bf
Heyang Liu <lhypds@gmail.com>
2023-05-22 16:38:48 +0000

    Use row loader to load markdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactMarkdown from 'react-markdown';
+import Timeline from './Timeline.md';
 
 const App = () =>{
   return (
-    <ReactMarkdown>
-*(Building in progress...)*  
-    </ReactMarkdown>
+    <>
+      <ReactMarkdown>
+        *(Building in progress...)*  
+      </ReactMarkdown>
+      <ReactMarkdown children={`${Timeline}`} />
+    </>
   )
 }
 
