840acb96fccd6604b3a12d474c13349bc936e478
LIU Heyang <lhypds@gmail.com>
2023-06-03 21:16:31 +0900

    Add redirection support

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import Timeline from "./projects/Timeline.md";
 import SimpleAiChat from "./projects/Simple AI Chat.md";
 import PlainTextNote from "./projects/Plain Text Note.md";
+import UnixNote from "./note/.markdown/Unix Note.md";
 
 const App = () => {
+  const [contentView, setContentView] = useState("null");
+
+  useEffect(() => {
+    window.addEventListener('changeContentView', (e) => {
+      setContentView(e.detail);
+    });
+  }, []);
+
   return (
     <>
       <ReactMarkdown>
         *(Building in progress...)*  
       </ReactMarkdown>
-      <ReactMarkdown children={`${Timeline}`} />
-      <ReactMarkdown children={`${SimpleAiChat}`} />
-      <ReactMarkdown children={`${PlainTextNote}`} />
+      {contentView === "null" ? 
+        (
+          <div>
+            <ReactMarkdown children={`${Timeline}`} />
+            <ReactMarkdown children={`${SimpleAiChat}`} />
+            <ReactMarkdown children={`${PlainTextNote}`} />
+          </div>
+        ) : (
+          <div>
+            <ReactMarkdown children={`${UnixNote}`} />
+          </div>
+        )}
     </>
   )
 }
