1aef66e3bc58d084b2f487af98a057a68ca13dae
Heyang Liu <lhypds@gmail.com>
2023-06-27 15:04:43 +0000

    Add content file server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,37 @@
 require('dotenv').config();
-
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('gcc3')
+})
+
+app.get('/note/:note', (req, res) => {
+  const note = req.params.note;
+  fs.readFile('./src/note/.markdown/' + note + " Note.md", 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err);
+        res.status(500).send('An error occurred while reading the markdown file.');
+    } else {
+        res.send(data);
+    }
+  });
+})
+
+app.get('/projects/:project', (req, res) => {
+  const project = req.params.project.replaceAll("_", " ");
+  fs.readFile('./src/projects/' + project + ".md", 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err);
+        res.status(500).send('An error occurred while reading the markdown file.');
+    } else {
+        res.send(data);
+    }
+  });
 })
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}...`)
 })
