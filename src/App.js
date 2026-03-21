7ef94bad0d7a181806ca38cc5066aad3c8964f49
Heyang Liu <lhypds@gmail.com>
2026-03-21 16:23:41 +0900

    Move Sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Content from "./components/Content/Content";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
@@ -38,38 +39,11 @@ const App = () => {
   return (
     <div className="wrapper wrapper-inline-block">
       {!isSidebarCollapsed && (
-        <div className="sidebar">
-          <div className="title">
-            <a href="https://www.gcc3.com" style={{ textDecoration: "none" }}>
-              <h1 style={{ display: "inline" }}>GCC</h1>
-              <div style={{ display: "inline", verticalAlign: "top" }}>3</div>
-              <div id="label-lab">lab</div>
-            </a>
-          </div>
-
-          <div style={{ display: "flex", justifyContent: "space-between", height: "38px" }}>
-            <h4>{category}</h4>
-            <h5
-              id="btn-collapse-sidbar"
-              className="btn-collapse-index"
-              onClick={() => setIsSidebarCollapsed(true)}
-            >
-              {"<<"}
-            </h5>
-          </div>
-
-          {notes.map(note => (
-            <p key={note}>
-              <a className="subject" href={`#note`}>{note}</a>
-            </p>
-          ))}
-
-          <h4>links</h4>
-          <p>
-            <a href="https://github.com/gcc3"> GitHub </a>
-          </p>
-          <a href="https://twitter.com/318yang">@318yang</a>
-        </div>
+        <Sidebar
+          category={category}
+          notes={notes}
+          onCollapse={() => setIsSidebarCollapsed(true)}
+        />
       )}
 
       <div
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Sidebar = ({ category, notes, onCollapse }) => {
+  return (
+    <div className="sidebar">
+      <div className="title">
+        <a href="https://www.gcc3.com" style={{ textDecoration: "none" }}>
+          <h1 style={{ display: "inline" }}>GCC</h1>
+          <div style={{ display: "inline", verticalAlign: "top" }}>3</div>
+          <div id="label-lab">lab</div>
+        </a>
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "space-between", height: "38px" }}>
+        <h4>{category}</h4>
+        <h5
+          id="btn-collapse-sidbar"
+          className="btn-collapse-index"
+          onClick={onCollapse}
+        >
+          {"<<"}
+        </h5>
+      </div>
+
+      {notes.map(note => (
+        <p key={note}>
+          <a className="subject" href="#note">{note}</a>
+        </p>
+      ))}
+
+      <h4>links</h4>
+      <p>
+        <a href="https://github.com/gcc3"> GitHub </a>
+      </p>
+      <a href="https://twitter.com/318yang">@318yang</a>
+    </div>
+  );
+};
+
+export default Sidebar;
